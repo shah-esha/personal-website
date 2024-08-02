4d891481aa61102b102d34abb83fbf6af25b3eee
Esha Shah <111465225+shah-esha@users.noreply.github.com>
2024-08-02 06:44:37 +0000

    finalize header and nav-bar

---
+++ main.js
@@ -0,0 +1,26 @@
+/*-------------------- MANUAL MENU OPEN/CLOSE --------------------*/
+const navMenu = document.getElementById('nav-menu'), 
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*-------------------- AUTOMATIC MENU CLOSE --------------------*/
+const navLink = document.querySelectorAll('.nav-link')
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
