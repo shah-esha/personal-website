6f589efd4e601e9bd4acb4f3bff09fcfe9bfe74a
Esha Shah <111465225+shah-esha@users.noreply.github.com>
2024-08-07 04:28:56 +0000

    blur content behind header

--- main.js
+++ main.js
@@ -24,3 +24,10 @@ const linkAction = () => {
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+const blurHeader = () => {
+    const header = document.getElementById('header')
+    this.scrollY >= 0 ? header.classList.add('blur-header') : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
