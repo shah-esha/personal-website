e1aeedbbc922de88ceffce5b2e8d8f750dc70b60
Esha Shah <111465225+shah-esha@users.noreply.github.com>
2024-08-13 17:03:35 +0000

    update skills

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
 /*-------------------- MANUAL MENU OPEN/CLOSE --------------------*/
 const navMenu = document.getElementById('nav-menu'), 
       navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+      navClose = document.getElementById('nav-close');
 
 if (navToggle) {
     navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu')
-    })
+        navMenu.classList.add('show-menu');
+    });
 }
 
 if(navClose) {
     navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu')
-    })
+        navMenu.classList.remove('show-menu');
+    });
 }
 
 /*-------------------- AUTOMATIC MENU CLOSE --------------------*/
-const navLink = document.querySelectorAll('.nav-link')
+const navLink = document.querySelectorAll('.nav-link');
 
 const linkAction = () => {
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show-menu');
 }
 
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
 
 const blurHeader = () => {
-    const header = document.getElementById('header')
-    this.scrollY >= 0 ? header.classList.add('blur-header') : header.classList.remove('blur-header')
+    const header = document.getElementById('header');
+    this.scrollY >= 0 ? header.classList.add('blur-header') : header.classList.remove('blur-header');
 }
-window.addEventListener('scroll', blurHeader)
+window.addEventListener('scroll', blurHeader);
